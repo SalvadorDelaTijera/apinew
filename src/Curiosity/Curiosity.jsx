b5272908649cce731f7659e039372d7405d94e4d
Salvador De la T <stijera@MacBook-Pro-de-stijera.local>
2024-02-21 21:20:01 -0600

    comit tailwind

--- src/Curiosity/Curiosity.jsx
+++ src/Curiosity/Curiosity.jsx
@@ -2,15 +2,15 @@ import React from 'react'
 
 const Curiosity = ({image, fullname, name, Id}) => {
   return (
-    <>
-    <div>
-      <img src={image} alt="" />
-      <h3>{fullname}</h3>
-      <h5>{name}</h5>
-      <p>{Id}</p>
+    <div className=''>
+    <div className=''>
+      <img className='' src={image} alt="" />
+      <h3 className=''>{fullname}</h3>
+      <h5 className=''> {name}</h5>
+      <p className=''>{Id}</p>
     </div>
 
-    </>
+    </div>
   )
 }
 
